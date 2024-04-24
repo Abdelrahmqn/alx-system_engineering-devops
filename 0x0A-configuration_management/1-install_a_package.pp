ed84b8a6e6c081bbe9875463d08f41c1a982b420
Abdelrahamqn <boda8099@gmail.com>
2024-04-24 11:51:53 +0200

    d

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # install flask from pip3 _ python
 # werkuezug now is 2.0.3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => '/usr/bin/pip3 install flask==2.1.0 ; pip3 install werkzeug==2.0.3',
   path    => ['/usr/bin'],
 }
