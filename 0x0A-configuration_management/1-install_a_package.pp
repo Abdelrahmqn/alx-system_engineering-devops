b4888470cbbd0492d744a4ccef455fd46bd42342
Abdelrahamqn <boda8099@gmail.com>
2024-04-24 11:44:34 +0200

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,4 @@
-#!/usr/bin/pup
-# Based on requirements install
-
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
 }
