866812292fae4c3836c520593c69d69d967e82ef
Abdelrahman <boda8099@gmail.com>
2024-06-13 16:32:00 +0300

    g:

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # make even more requestsss!!
 
 exec { 'fix--for-nginx':
-  command     => '/bin/sed -i "s/15/4096/" /etc/default/nginx,
+  command     => '/bin/sed -i "s/15/4096/" /etc/default/nginx.conf,
   path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
 
