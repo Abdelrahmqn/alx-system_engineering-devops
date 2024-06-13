7f8a6bf0fe7de2be07e3787e0f44db49a1d382b3
Abdelrahman <boda8099@gmail.com>
2024-06-13 16:19:17 +0300

    0x1B web stack debugging_4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# make even more requestsss!!
+
+exec { 'fix--for-nginx':
+  command     => '/bin/sed -i "s/15/4096/" /etc/default/nginx,
+  path        => '/bin:/usr/bin',
+}
+
+exec { 'nginx-restart':
+  command => '/etc/init.d/nginx restart',
+  path    => '/etc/init.d/',
+}
---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# give a permision for holberton
+
+
+exec { 'give-soft-permisions-forholperton':
+  command => "sed -i '^/"holberton soft/s/4/5000/' /etc/security/limits.conf",
+  path    => "/usr/local/bin/:/bin/'
+}
+
+
+exec { 'give-hard-permisions-forholperton':
+  command => "sed -i '^/"holberton hard/s/5/5000/' /etc/security/limits.conf",
+  path    => "/usr/local/bin/:/bin/'
+}
