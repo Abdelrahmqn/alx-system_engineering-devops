6632596998bb47c76ebbfb8ebeae69bc5a6f28f7
Abdelrahman <boda8099@gmail.com>
2024-06-13 16:57:05 +0300

    add

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,8 @@
 # make even more requestsss!!
 
-exec { 'fix--for-nginx':
-  command     => '/bin/sed -i "s/15/4096/" /etc/default/nginx.conf,
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/3000/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
-exec { 'nginx-restart':
-  command => '/etc/init.d/nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
-}
+
